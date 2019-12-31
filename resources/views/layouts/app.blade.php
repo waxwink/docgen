4142e3fbe880f0820d249b89c80cb27dcb938d23
Mohamadreza <darkknight_89@yahoo.com>
2019-12-31 15:55:25 +0330

    route controller got corrected

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Pinwork') }}</title>
+    <title>{{ config('app.name', 'App') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Pinwork\DocGen\Http\Controller\DocumentationController;
+use Waxwink\DocGen\Http\Controller\DocumentationController;
+
 
 Route::get('/responses', DocumentationController::class . '@responses')->middleware('document.auth');
 Route::get('/routes', DocumentationController::class . '@routes')->middleware('document.auth');
